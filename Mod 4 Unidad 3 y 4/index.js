6db7b707a7cdfe6dba61d7aae78e67360c7282e4
Joaquin Sosa Belaustegui <joaquinsb@icloud.com>
2025-04-06 17:56:21 -0300

    rutasDinamicasyEstaticasEnUnaAplicacionSencilla

--- Mod 4 Unidad 3 y 4/index.js
+++ Mod 4 Unidad 3 y 4/index.js
@@ -2,18 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Ruta raíz. La que me aparece cuando abro el puerto local.
 app.get('/', (req, res) => {
-  res.send('¡Bienvenido a mi aplicación Express! Mi nombre es Joaquin. ');
+  res.send('Usá la ruta /saludo/tu-nombre para recibir un saludo. También podés visitar /informacion o /contacto.');
 });
 
-app.get('/info', (req, res) => {
-  res.send('Prueba #1. Esta es la sección de información.');
+// Ruta dinámica con parámetro en la URL
+app.get('/saludo/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+
+  if (nombre.toLowerCase() === 'admin') {
+    res.send('Hola Admin 👑. Tenés acceso privilegiado.');
+  } else {
+    res.send(`Hola ${nombre}, ¡bienvenido/a a Express!`);
+  }
 });
 
+// Página de información. Funciona IUJUUU esta fue la primera que hice estatica.
+app.get('/informacion', (req, res) => {
+  res.send('Esta es la sección de información general sobre nuestra app Express.');
+});
+
+// Página de contacto
 app.get('/contacto', (req, res) => {
-  res.send('Prueba #2. Podés contactarme a joaquinsb@icloud.com');
+  res.send('Podés contactarnos por correo a contacto@ejemplo.com');
 });
 
+//Esto vi ejemplos que lo suaban pero no termino de entender la funcion, sera solo para que aparezca en consola?
 app.listen(port, () => {
-  console.log(`Prueba #3. Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
